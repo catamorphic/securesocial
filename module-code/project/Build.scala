cb161a95949c2924aa09323e73d32c6bb74c3f90
John Kodumal <jkodumal@gmail.com>
2013-04-21 18:03:34 -0700

    Bump to play 2.1.1

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -8,8 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "master-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.typesafe" %% "play-plugins-util" % "2.1.0",
-      "com.typesafe" %% "play-plugins-mailer" % "2.1.0",
+      "com.typesafe" %% "play-plugins-util" % "2.1.1",
+      "com.typesafe" %% "play-plugins-mailer" % "2.1.1",
       "org.mindrot" % "jbcrypt" % "0.3m"
     )
 
